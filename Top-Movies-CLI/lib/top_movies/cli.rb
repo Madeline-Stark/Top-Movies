0bca8673b2a80cdedea08f414a967632a447604b
Madeline Stark <madeline.stark1@gmail.com>
2018-07-27 20:43:35 +0000

    Build out menu method.

--- Top-Movies-CLI/lib/top_movies/cli.rb
+++ Top-Movies-CLI/lib/top_movies/cli.rb
@@ -16,28 +16,27 @@ def menu
   input = nil
   puts "Your options are to 1. See the top 250 movies of IMDB. 2. See if your favorite movie is on the list."
   puts "Please enter the number of the option you're interested in or type exit to leave:"
- 
-# When they choose see imdb top 250 they will see a list actors scraped from imdb's top 250. 
-#Once they’ve chosen this, they will be 
-# prompted again to see if they’d like to see the list again or go back to the main menu. 
-# if number 2 ask what is your favorite movie? if movie included method from movie class is true,
-#say congrats! your favorite movie is a critical darling! or: Sorry! You must have bad taste in movies! 
 
-#     while input != "exit"
-#  
-#       input = gets.strip.downcase
-# 
-#       if input.to_i > 0
-#         the_deal = @deals[input.to_i-1]
-#         puts "#{the_deal.name} - #{the_deal.price} - #{the_deal.availability}"
-#       elsif input == "list"
-#         list_deals
-#       else
-#         puts "Not sure what you want, type list or exit."
-#       end
-#     end
-#   end
-# 
+
+    while input != "exit"
+ 
+      input = gets.strip.downcase
+
+      if input.to_i == 1
+        make_movies
+        display_movies
+      elsif input == 2
+          puts "What's your favorite movie?"
+          movie = gets.strip.downcase 
+          favorite_movie(movie)
+       else
+         puts "Sorry, that's not an option."
+         menu 
+      end
+     end
+    menu 
+
+  end
 
   def make_movies
       movies_array = Scraper.scrape_index_page
@@ -48,6 +47,10 @@ def menu
       Movies.all.each_with_index(1) do |movie, index|
         puts "#{index}.) : ""#{movie.name.upcase}"
   end
+
+  def favorite_movie(movie)
+    Movies.movie_included(movie)
+  end 
 
   def goodbye
     puts "See you later!"
