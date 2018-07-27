0e71828d97bc461cded4c91276dccc21f3c3b03f
Madeline Stark <madeline.stark1@gmail.com>
2018-07-27 19:48:36 +0000

    Build basic structure of Movie class

--- Top-Movies-CLI/lib/top_movies/movies.rb
+++ Top-Movies-CLI/lib/top_movies/movies.rb
@@ -1,7 +1,16 @@
 class Movies
+  attr_accessor :name
 
-def initialize
+  @@all = [ ]
 
+def initialize(scraped_data)
+  #takes in scraped data from scraper find movies method and creates instance of movie class
+  #sets movie.name to object to string 
+  #pushes self into all
+end 
+
+def self.all
+  @@all
 end 
 
 end
