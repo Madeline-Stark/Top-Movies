cdd65f75d10b4fbdf59e450d7bfdf6c9fc823243
Madeline Stark <madeline.stark1@gmail.com>
2018-07-27 20:06:36 +0000

    Add create from collection method

--- Top-Movies-CLI/lib/top_movies/movies.rb
+++ Top-Movies-CLI/lib/top_movies/movies.rb
@@ -3,12 +3,19 @@ class Movies
 
   @@all = [ ]
 
-def initialize(scraped_data)
+def initialize(movie)
   #takes in scraped data from scraper find movies method and creates instance of movie class
   #or maybe calls on scraper class here?
   #sets movie.name to object to string 
   #pushes self into all
 end 
+
+def self.create_from_collection(movies_array)
+      movies_array.each do |movie|
+        movie = Movie.new(movie)
+      end
+end
+
 
 def self.all
   @@all
