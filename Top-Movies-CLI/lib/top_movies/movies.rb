86408603751607815739fea3c7b4a0fb04c19272
Madeline Stark <madeline.stark1@gmail.com>
2018-07-30 19:33:15 +0000

    Write some notes.

--- Top-Movies-CLI/lib/top_movies/movies.rb
+++ Top-Movies-CLI/lib/top_movies/movies.rb
@@ -17,46 +17,30 @@ end
       
 
 def self.movie_included(movie)
-  if self.all.include?(movie)
+  if self.all.include?(movie) #do movie.strip.downcase to all and to movie? 
     puts "Congrats! Your favorite movie is a fan favorite!"
   else
     puts "Sorry! You chose an unpopular movie."
   end
 end
 
-def self.show_movie_number(number)
+def self.list_movies
   new_array = []
-  Movies.all.each_with_index(1) do |movie, index|
-    new_array << {index => movie}
+  Movies.all.each_with_index do |movie, index|
+    newer_array << ("#{index + 1}.) #{movie}")
   end
-  new_array[number]
+  new_array
 end
-  #do each with index in hash and then read out that number
-  #need to iterate over array with each with index
 
-def self.favorite_movie(movie)
-  Movies.movie_included(movie)
+
+def self.show_movie_number(number)
+  list_movies #will we have access to new_array outside of list_movies method? maybe list_movies[number - 1]
+  new_array[number - 1]
 end
 
 
 def self.all
   @@all
 end
-
-# For testing:
-# all = [ ]
-#       new_array.each do |movie|
-#         all << movie
-#       end #works!
-#       
-#       film = "Superbad"
-#       film = "The Godfather"
-#       film = "Catch Me If You Can"
-#         if all.include?(film)
-#           puts "Congrats! Your favorite movie is a fan favorite!"
-#         else
-#           puts "Sorry! You chose an unpopular movie."
-#         end #works! 
-
 
 end
