97027aa6d66003758aab2f09179d92ebceed326f
Madeline Stark <madeline.stark1@gmail.com>
2018-08-02 18:34:26 +0000

    Changes scrape_index_page to use hash.

--- Top-Movies-CLI/lib/top_movies/scraper.rb
+++ Top-Movies-CLI/lib/top_movies/scraper.rb
@@ -4,28 +4,26 @@ require "pry"
 
 module TopMovies
 class Scraper
+  attr_accessor :title, :date
 
 
   def initialize
-    self.scrape_top_page
+    self.scrape_index_page
   end
 
 
   def self.scrape_index_page
     doc = Nokogiri::HTML(open("https://www.imdb.com/chart/top"))
     new_array = []
-    binding.pry
-    #maybe do year it was made with attributed text
-  #film = Movie.new(name, date)
-      #doc.css('span.secondaryInfo')[5].text give us year!!
     doc.css('td.titleColumn a').each_with_index do |movie, index|
-      movie = doc.css('td.titleColumn a')[index].text
-      new_array << movie
+      movie_title = doc.css('td.titleColumn a')[index].text
+      movie_date = doc.css('span.secondaryInfo')[5].text
+      new_array << {:title => movie_title, :date => movie_date}
       #each individual movie should be its own hash with key value pairs like title, director, lead actors etc
       #then when instantiating movie can use data from hash to set attributes(pass them into initialize)
       #and be able to call on cli
       #can also initialize movie here
-      #important that where initializing objects with attributes
+      #important that were initializing objects with attributes
     end
     new_array
   end
