61190682986a41f932bd64760ffca8ba1cb152b2
Madeline Stark <madeline.stark1@gmail.com>
2018-08-02 19:52:52 +0000

    Add Spec checklist.

--- Top-Movies-CLI/lib/top_movies/cli.rb
+++ Top-Movies-CLI/lib/top_movies/cli.rb
@@ -14,7 +14,6 @@ end
 def menu
   input = nil
   puts "Your options are to 1. See the top 250 movies of IMDB. 2.See the release date of a specific movie. 3.See if your favorite movie is on the list."
-  # 3.See the movie corresponding to a certain number
   puts "Please enter the number of the option you're interested in or type exit to leave:"
 
 
@@ -34,25 +33,13 @@ def menu
           movie = gets.strip
           Movies.movie_included(movie)
           menu
-      # elsif input.to_i == 3
-      #   puts "What number movie are you interested in?"
-      #   number = gets.strip.to_i
-      #   Movies.show_movie_number(number)
-      #   menu
     elsif input == "exit"
       puts "See you later!"
        else
          puts "Sorry, that's not an option."
          menu
       end
-     #should go to goodbye after this as is called in run
   end
-
-  # def display_movies
-  #     Movies.all.each_with_index(1) do |movie, index|
-  #       puts "#{index}.) : ""#{movie.name.upcase}"
-  #     end
-  # end
 
 
 end
